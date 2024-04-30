c713e126812ad69f4de05fc1fdecbe5d79fe91da
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-04-30 06:18:27 -0300

    feat: :sparkles: added validators to osfilesouce
    introduced validators to enhance osfilesouce path validation

--- internal/application/service/os_file_source.go
+++ internal/application/service/os_file_source.go
@@ -1,7 +1,12 @@
 package service
 
+import (
+	"github.com/mateusmacedo/govibranium/prototype/internal/core/validation"
+)
+
 type OSFileSource struct {
 	path string
+	validators []validation.Validator
 }
 
 type OSFileSourceOption func(*OSFileSource) error
@@ -21,7 +26,19 @@ func WithOSFileSourcePath(path string) OSFileSourceOption {
 	}
 }
 
+func WithOSFileSourceValidators(validators ...validation.Validator) OSFileSourceOption {
+	return func(s *OSFileSource) error {
+		s.validators = validators
+		return nil
+	}
+}
+
 func NewOSFileSource(opts ...OSFileSourceOption) (*OSFileSource, error) {
 	s := OSFileSourceOptionsFunc(opts...)
+	for _, v := range s.validators {
+		if err := v.Validate(s.path); err != nil {
+			return nil, err
+		}
+	}
 	return s, nil
 }
