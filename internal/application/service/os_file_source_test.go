da566107221ba440b7228463fb4a42ec7f379994
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-04-30 06:18:45 -0300

    test: :test_tube: added invalid path handling test
    expanded unit tests to cover scenario with invalid file path, ensuring error handling works as expected.

--- internal/application/service/os_file_source_test.go
+++ internal/application/service/os_file_source_test.go
@@ -1,7 +1,12 @@
 package service
 
 import (
+	"fmt"
 	"testing"
+
+	"github.com/golang/mock/gomock"
+
+	"github.com/mateusmacedo/govibranium/prototype/test/mocks"
 )
 
 func TestOSFileSource(t *testing.T) {
@@ -13,6 +18,22 @@ func TestOSFileSource(t *testing.T) {
 			}
 			if s.path != "test" {
 				t.Errorf("Expected path to be test, got %s", s.path)
+			}
+		})
+		t.Run("Test NewOSFileSource with invalid path", func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+    		defer ctrl.Finish()
+			mockValidator := mocks.NewMockValidator(ctrl)
+			mockValidator.EXPECT().Validate("test").Return(fmt.Errorf("error"))
+			_, err := NewOSFileSource(
+				WithOSFileSourcePath("test"),
+				WithOSFileSourceValidators(mockValidator),
+			)
+			if err == nil {
+				t.Error("Expected error, got nil")
+			}
+			if err.Error() != "error" {
+				t.Errorf("Expected error to be error, got %s", err.Error())
 			}
 		})
 	})
