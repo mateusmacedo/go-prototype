bcfffd1fbff97b7a6346e4d11516dc40eba9f2a0
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-04-30 16:18:47 -0300

    test: :test_tube: enhanced os_file_source tests
    added validation and open method tests, improving coverage and reliability

--- internal/application/service/os_file_source_test.go
+++ internal/application/service/os_file_source_test.go
@@ -11,34 +11,97 @@ import (
 
 func TestOSFileSource(t *testing.T) {
 	t.Run("Test NewOSFileSource", func(t *testing.T) {
-		t.Run("Test NewOSFileSource with valid path", func(t *testing.T) {
-			ctrl := gomock.NewController(t)
+		t.Run("Test NewOSFileSource with valid path", func(tr *testing.T) {
+			ctrl := gomock.NewController(tr)
     		defer ctrl.Finish()
-			mockAdapter := mocks.NewMockSource(ctrl)
-			mockAdapter.EXPECT().Open().Return("test", nil)
-			_, err := NewOSFileSource(
-				WithOSFileSourcePath("test"),
-				WithOSFileSourceAdapter(mockAdapter),
-			)
-			if err != nil {
-				t.Errorf("Expected no error, got %s", err)
-			}
-		})
-		t.Run("Test NewOSFileSource with invalid path", func(t *testing.T) {
-			ctrl := gomock.NewController(t)
-    		defer ctrl.Finish()
+			expValidate := interface{}(nil)
 			mockValidator := mocks.NewMockValidator(ctrl)
-			expectedErr := err.ErrorFactory(InvalidOSFileSourcePathErrMsg, "test")
-			mockValidator.EXPECT().Validate("test").Return(expectedErr)
+			mockValidator.EXPECT().Validate("test").Return(expValidate)
+			mockAdapter := mocks.NewMockSource(ctrl)
 			_, err := NewOSFileSource(
 				WithOSFileSourcePath("test"),
 				WithOSFileSourceValidators(mockValidator),
+				WithOSFileSourceAdapter(mockAdapter),
+			)
+			if err != nil {
+				tr.Errorf("Expected no error, got %s", err)
+			}
+		})
+		t.Run("Test NewOSFileSource with invalid path", func(tr *testing.T) {
+			ctrl := gomock.NewController(tr)
+    		defer ctrl.Finish()
+			expValidate := err.ErrorFactory(InvalidOSFileSourcePathErrMsg, "test")
+			mockValidator := mocks.NewMockValidator(ctrl)
+			mockValidator.EXPECT().Validate("test").Return(expValidate)
+			mockAdapter := mocks.NewMockSource(ctrl)
+			_, err := NewOSFileSource(
+				WithOSFileSourcePath("test"),
+				WithOSFileSourceValidators(mockValidator),
+				WithOSFileSourceAdapter(mockAdapter),
 			)
 			if err == nil {
-				t.Error("Expected error, got nil")
+				tr.Error("Expected error, got nil")
 			}
-			if err.Error() != expectedErr.Error() {
-				t.Errorf("Expected error to be %s, got %s", expectedErr, err)
+			if err.Error() != expValidate.Error() {
+				tr.Errorf("Expected error to be %s, got %s", expValidate, err)
+			}
+		})
+	})
+	t.Run("Test OSFileSource Open", func(t *testing.T) {
+		t.Run("Test OSFileSource Open with valid adapter", func(tr *testing.T) {
+			ctrl := gomock.NewController(tr)
+    		defer ctrl.Finish()
+			expValidate := interface{}(nil)
+			mockValidator := mocks.NewMockValidator(ctrl)
+			mockValidator.EXPECT().Validate("test").Return(expValidate)
+			expOpen := interface{}(nil)
+			mockAdapter := mocks.NewMockSource(ctrl)
+			mockAdapter.EXPECT().Open().Return(nil, expOpen)
+			s, err := NewOSFileSource(
+				WithOSFileSourcePath("test"),
+				WithOSFileSourceValidators(mockValidator),
+				WithOSFileSourceAdapter(mockAdapter),
+			)
+			if err != nil {
+				tr.Errorf("Expected no error, got %s", err)
+			}
+
+			p, err := s.Open()
+			if err != nil {
+				tr.Errorf("Expected no error, got %s", err)
+			}
+
+			if p != expOpen {
+				tr.Errorf("Expected %v, got %v", expOpen, p)
+			}
+		})
+		t.Run("Test OSFileSource Open with invalid adapter", func(tr *testing.T) {
+			ctrl := gomock.NewController(tr)
+			defer ctrl.Finish()
+			expValidate := interface{}(nil)
+			mockValidator := mocks.NewMockValidator(ctrl)
+			mockValidator.EXPECT().Validate("test").Return(expValidate)
+			expOpen := err.ErrorFactory("error %s", "test")
+			mockAdapter := mocks.NewMockSource(ctrl)
+			mockAdapter.EXPECT().Open().Return(nil, expOpen)
+			s, err := NewOSFileSource(
+				WithOSFileSourcePath("test"),
+				WithOSFileSourceValidators(mockValidator),
+				WithOSFileSourceAdapter(mockAdapter),
+			)
+			if err != nil {
+				tr.Errorf("Expected no error, got %s", err)
+			}
+
+			p, err := s.Open()
+			if err == nil {
+				tr.Error("Expected error, got nil")
+			}
+			if err.Error() != expOpen.Error() {
+				tr.Errorf("Expected error to be %s, got %s", expOpen, err)
+			}
+			if p != nil {
+				tr.Errorf("Expected nil, got %v", p)
 			}
 		})
 	})
